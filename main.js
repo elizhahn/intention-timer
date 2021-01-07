7b95bbd74c010e2db98ddbfcb2662f1605635977
Elizabeth Hahn <elizhahn87@gmail.com>
2021-01-07 14:23:14 -0700

    Setup function to hide form, and display timer

---
+++ main.js
@@ -0,0 +1,12 @@
+var buttonStartActivity = document.querySelectory(".activity-button");
+var form = document.querySelector(".form-container");
+var timer = document.querySelector(".timer-container");
+
+
+buttonStartActivity.addEventListener("click", );
+
+
+function displayTimer() {
+  form.classList.add("hidden");
+  timer.classList.remove("hidden");
+}
