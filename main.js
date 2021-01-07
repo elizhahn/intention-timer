fff40c32d53bb203b516334cde25a8d3b00f1cb8
Owen Hallgren <owenhallgren@gmail.com>
2021-01-06 21:00:35 -0700

    Started feature to prevent invalid inputs

--- main.js
+++ main.js
@@ -2,16 +2,33 @@ var iconDeactivated = document.querySelectorAll(".deactivated");
 var iconActivated = document.querySelectorAll(".activated");
 var categoryInputs = document.querySelectorAll(".category-input");
 var categoryContainer = document.querySelector(".category-container");
-
+var inputMinutes = document.getElementById("minutes");
+var inputSeconds = document.getElementById("seconds");
+var buttonStartActivity = document.querySelector(".activity-button")
 
 categoryContainer.addEventListener("click", activateIcon);
+buttonStartActivity.addEventListener("click", validateNumber);
 
-function activateIcon(event) {
+function preventDefault(e){
+  e.preventDefault();
+}
+
+function validateNumber(){
+  if(typeof(inputMinutes.value) !== "number" || typeof(inputSeconds) !== "number") {
+    alert("yes");
+  }
+};
+
+function activateIcon() {
+  displayActivatedIcon(event);
+}
+
+function displayActivatedIcon(event) {
   console.log(event.target);
   for(var i = 0; i < categoryInputs.length; i++) {
     if(categoryInputs[i].checked) {
       iconDeactivated[i].classList.toggle("hidden");
       iconActivated[i].classList.toggle('hidden');
-    }
-  }
-}
+    };
+  };
+};
