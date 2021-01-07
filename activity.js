004c644a828a077206d73054bba187a7ceb90665
Owen Hallgren <owenhallgren@gmail.com>
2021-01-06 19:11:34 -0700

    Added Activity class and methods

---
+++ activity.js
@@ -0,0 +1,18 @@
+class Activity {
+  constructor(category, description, minutes, seconds, completed, id) {
+    this.category = category;
+    this.description = description;
+    this.seconds = minutes;
+    this.completed = seconds;
+    this.id = id;
+  };
+  countdown(){
+    console.log();
+  };
+  markComplete(){
+    console.log();
+  };
+  saveToStorage(){
+    console.log();
+  };
+}
