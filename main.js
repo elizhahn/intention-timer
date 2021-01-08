5a9e23876d57e541887c48490e03463584d21388
Elizabeth Hahn <elizhahn87@gmail.com>
2021-01-07 18:47:59 -0700

    Adjust class names for DOM selectors

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-var buttonStartActivity = document.querySelectory(".activity-button");
-var form = document.querySelector(".form-container");
+var buttonStartActivity = document.querySelector(".activity-button");
+var form = document.querySelector("form");
 var timer = document.querySelector(".timer-container");
 
 
-buttonStartActivity.addEventListener("click", );
+buttonStartActivity.addEventListener("click", displayTimer);
 
 
-function displayTimer() {
+
+
+function displayTimer(e) {
+  e.preventDefault();
   form.classList.add("hidden");
   timer.classList.remove("hidden");
 }
