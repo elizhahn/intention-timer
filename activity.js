04ee5214bde466262b996db4c3cf8b6ee256d41b
Owen Hallgren <owenhallgren@gmail.com>
2021-01-07 19:08:39 -0700

    Fixed merge conflicts for feature/form

--- activity.js
+++ activity.js
@@ -2,14 +2,9 @@ class Activity {
   constructor(category, description, minutes, seconds, completed, id) {
     this.category = category;
     this.description = description;
-<<<<<<< HEAD
-    this.seconds = minutes;
-    this.completed = seconds;
-=======
     this.minutes = minutes;
     this.seconds = seconds;
     this.completed = completed;
->>>>>>> main
     this.id = id;
   };
   countdown(){
--- main.js
+++ main.js
@@ -1,32 +1,6 @@
 var iconDeactivated = document.querySelectorAll(".deactivated");
 var iconActivated = document.querySelectorAll(".activated");
 var categoryInputs = document.querySelectorAll(".category-input");
-<<<<<<< HEAD
-var categoryContainer = document.querySelector(".category-container");
-var inputMinutes = document.getElementById("minutes");
-var inputSeconds = document.getElementById("seconds");
-var buttonStartActivity = document.querySelector(".activity-button")
-
-categoryContainer.addEventListener("click", activateIcon);
-inputMinutes.addEventListener("keyup", validateNumberMinutes)
-inputSeconds.addEventListener("keyup", validateNumberSeconds)
-
-function preventDefault(e){
-  e.preventDefault();
-}
-
-function validateNumberMinutes(e){
-  if (!(e.keyCode >= 48 && e.keyCode <= 57) && !(e.keyCode >= 96 && e.keyCode <= 105) && !(e.keyCode == 8)){
-    e.target.value = e.target.value.substring(0, e.target.value.length - 1);
-  };
-};
-
-function validateNumberSeconds(e){
-  if (!(e.keyCode >= 49 && e.keyCode <= 57) && !(e.keyCode >= 96 && e.keyCode <= 105) && !(e.keyCode == 8)){
-    e.target.value = e.target.value.substring(0, e.target.value.length - 1);
-  };
-};
-=======
 var categoryButtons = document.querySelectorAll(".category-label");
 var categoryContainer = document.querySelector(".category-container");
 var inputActivity = document.getElementById('activity');
@@ -41,24 +15,28 @@ var createdActivities = [];
 
 categoryContainer.addEventListener("click", activateIcon);
 buttonStartActivity.addEventListener('click', submitForm);
+inputMinutes.addEventListener("keyup", validateNumberMinutes)
+inputSeconds.addEventListener("keyup", validateNumberSeconds)
 
->>>>>>> main
+
 
 function activateIcon() {
   displayActivatedIcon(event);
 }
 
-function displayActivatedIcon(event) {
-<<<<<<< HEAD
-  console.log(event.target);
-  for(var i = 0; i < categoryInputs.length; i++) {
-    if(categoryInputs[i].checked) {
-      iconDeactivated[i].classList.toggle("hidden");
-      iconActivated[i].classList.toggle('hidden');
-    };
+function validateNumberMinutes(e){
+  if (!(e.keyCode >= 48 && e.keyCode <= 57) && !(e.keyCode >= 96 && e.keyCode <= 105) && !(e.keyCode == 8)){
+    e.target.value = e.target.value.substring(0, e.target.value.length - 1);
   };
 };
-=======
+
+function validateNumberSeconds(e){
+  if (!(e.keyCode >= 49 && e.keyCode <= 57) && !(e.keyCode >= 96 && e.keyCode <= 105) && !(e.keyCode == 8)){
+    e.target.value = e.target.value.substring(0, e.target.value.length - 1);
+  };
+};
+
+function displayActivatedIcon(event) {
   for(var i = 0; i < categoryInputs.length; i++) {
     if(categoryInputs[i].checked) {
       iconActivated[i].classList.toggle('hidden');
@@ -100,4 +78,3 @@ function checkInputs() {
 function showErrorMessage(index) {
   errorMessages[index].classList.remove('hidden');
 }
->>>>>>> main
