2014ce34e320d6335793d822ed134979a3651779
Jackson McGuire <jaxmcguire@gmail.com>
2021-01-08 12:54:08 -0500

    Added start timer event listener

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ inputSeconds.addEventListener("keyup", validateNumberSeconds);
 form.addEventListener("submit", function() {
   event.preventDefault();
 })
-
+timerButton.addEventListener('click', startCountdown);
 
 function display(feature) {
   feature.classList.remove("hidden");
@@ -91,10 +91,9 @@ function validateForm() {
   }
   else {
     var createdActivity = new Activity(currentIcon.id, inputActivity.value, inputMinutes.value, inputSeconds.value);
-    createdActivities.push(createdActivity);
+    createdActivities.unshift(createdActivity);
     //we will need to call this method on the start button listener instead
     //need to take displaly funcitonality and move it
-    createdActivity.countdown();
     displayTimer();
   }
 }
@@ -110,7 +109,13 @@ function checkInputs() {
   return true;
 }
 
-
 function showErrorMessage(index) {
   errorMessages[index].classList.remove('hidden');
 }
+
+function startCountdown() {
+  createdActivities[0].countdown();
+}
+
+// <span class="warning-icon"> <svg id="Capa_1" enable-background="new 0 0 551.14 551.14" height="512" viewBox="0 0 551.14 551.14" width="512" xmlns="http://www.w3.org/2000/svg"><path d="m533.912 551.135h-516.684c-5.971 0-11.504-3.095-14.649-8.174s-3.431-11.403-.757-16.752l258.342-516.684c2.925-5.817 9.16-9.517 15.397-9.52 6.243-.003 12.488 3.697 15.415 9.52l258.342 516.685c2.674 5.349 2.388 11.672-.757 16.752-3.144 5.078-8.678 8.173-14.649 8.173zm-488.814-34.446h460.945l-230.474-460.944-230.473 460.944z" fill="#EFB7EC"/><path d="m258.347 206.678h34.446v172.228h-34.446z" fill="#EFB7EC"/><path d="m258.347 413.352h34.446v34.446h-34.446z" fill="#EFB7EC"/></svg> </span> A category is required</label>
+// .error-message { color: #EFB7EC; } .warning-icon { height: 10px; width: 5px; }
