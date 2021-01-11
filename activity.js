d1e9abfc320abc1167952589a776a965816bb335
Owen Hallgren <owenhallgren@gmail.com>
2021-01-11 10:55:09 -0700

    Fixed time decreasing by 2 intervals

--- activity.js
+++ activity.js
@@ -23,7 +23,6 @@ class Activity {
       var minutes = String(Math.trunc(time / 60)).padStart(2, 0)
       var seconds = String(Math.trunc(time % 60)).padStart(2, 0)
       timer.textContent = `${minutes}:${seconds}`;
-      time--;
       if(time === -1) {
         clearInterval(decreaseTime);
         this.timerRunning = false;
