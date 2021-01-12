ad1f767950e220407b39a7a4777657f33df3f13a
Elizabeth Hahn <elizhahn87@gmail.com>
2021-01-11 17:12:58 -0700

    Remove DOM manipulation from markComplete method

--- activity.js
+++ activity.js
@@ -36,27 +36,13 @@ class Activity {
   };
   };
   markComplete() {
-    var card = document.createElement("li");
-    pastActivityList.appendChild(card);
-    card.classList.add("past-activity-card");
-    card.innerHTML =
-    `<article class="card">
-        <li class="past-activity">
-         <p class="past-activity-category">${this.category.charAt(0).toUpperCase() + this.category.slice(1)}</p>
-         <div class="color-icon"></div>
-        </li>
-        <time class="past-activity-time">${this.minutes} MIN ${this.seconds} SECONDS</time>
-        <li class="past-activity-description">
-          <p>${this.description}</p>
-        </li>
-     </article>`;
-     pastActivities.push(this);
-     showCardMarkerColor();
+      
     }
 
   saveToStorage() {
     var savedActivity = JSON.stringify(this);
         if(localStorage.length === 0) {
+          //key = this.id
           var key = "storage " + 0;
           localStorage.setItem(key, savedActivity);
         } else {
